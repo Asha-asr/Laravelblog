0697aea67c9bfb4cb1f036375e15f63a13f0e167
Asha-asr <asharamankutty20@gmail.com>
2021-12-31 12:38:12 +0530

    FirstCommit:Firstersion

---
+++ resources/views/about.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class=" grid grid-cols-1 m-auto">
+        <div class="flex text-gray-100 pt-10">
+                    <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
+                        <h1 class="sm:text-black text-5xl uppercase fontbold text-shadow-md pb-14">
+                            About US
+
+                        </h1>  
+
+                        
+
+                    <div class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl ">
+                        <p>
+                        Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:
+
+                        “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.”
+                        The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.) that doesn't distract from the layout. A practice not without controversy, laying out pages with meaningless filler text can be very useful when the focus is meant to be on design, not content.
+                        </p>
+                        
+                    </div>
+
+                    </div>
+        </div>
+
+
+    </div>
+
+   
+
+   
+
+    
+
+
+@endsection
